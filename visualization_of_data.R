23504d5d8602526f531312a5fa3b5b7680534458
misaki002 <misaki002@users.noreply.github.com>
2016-05-12 11:30:31 +0800

    Update visualization_of_data.R

--- visualization_of_data.R
+++ visualization_of_data.R
@@ -1,7 +1,7 @@
 # visualization for relationship between bike rentals with other data fields 
 
 #set directory and read csv file
-setwd("D:\final\test4")
+setwd("D:\final\test5")
 train <- read.csv("train.csv")
 
 # numerical data
